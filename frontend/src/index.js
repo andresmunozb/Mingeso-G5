009ac2b707b1e46068015d01f41754f464257d72
Andrés Muñoz <andres.munoz.b@usach.cl>
2018-05-15 10:49:41 -0400

    Cambio de estilo de front

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,8 @@
-/* eslint-disable import/default */
-
 import React from 'react';
-import { render } from 'react-dom';
-import { Router, browserHistory } from 'react-router';
-import routes from './routes';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-require('./favicon.ico');
-import './styles.scss';
-import 'font-awesome/css/font-awesome.css';
-import 'flexboxgrid/css/flexboxgrid.css';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
 
-injectTapEventPlugin();
-
-render(
-    <Router routes={routes} history={browserHistory} />, document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
