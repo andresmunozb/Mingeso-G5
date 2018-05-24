1fd5c6959f9357259348058a97f716f4f1b05273
Unknown <andres.munoz.b@usach.cl>
2018-05-24 19:36:35 -0400

    Agregando rutas dinamicas (no terminado)

--- frontend/src/components/Global/Header.js
+++ frontend/src/components/Global/Header.js
@@ -1,43 +1,49 @@
 import React,{Component} from 'react';
-import {Navbar,Nav,MenuItem,NavDropdown} from 'react-bootstrap';
+import {Navbar,Nav,NavDropdown} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import HeaderIn from './HeaderIn';
 import HeaderOut from './HeaderOut';
 import PropTypes from 'prop-types';
 
 //import styles from './Header.css';
-
 class MyLink extends Component{
     render(){
         return(
-            <li ><Link to='/'>Home</Link></li>
+            <li key={this.props.key} >
+            {(this.props.route.enable) ?<Link to={this.props.route.path}>{this.props.route.title}</Link>:null}
+            </li>
+                  
         );
     }
 }
+
 class Header extends Component {
     render(){
         return (
          
             <Navbar inverse collapseOnSelect>
-                <Navbar.Header>
-                    <Navbar.Brand>
-                    <a href="#brand">React-Bootstrap</a>
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                </Navbar.Header>
+            <Navbar.Header>
+                <Navbar.Brand>
+                <a href="#brand">USACH</a>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+            </Navbar.Header>
                 <Navbar.Collapse>
+                
                     <Nav>
-                    <MyLink/>
+                    {this.props.routes && this.props.routes.map(
+                        (route,key) => 
+                            route.routes ?  
+                            <NavDropdown title={route.title} id="basic-nav-dropdown">
+                                {route.routes.map((route2,key2)=> <MyLink key={key2} route={route2}/>)}
+                            </NavDropdown>:
+                            <MyLink key={key} route={route}/>
+                    )}
                     
-                    <NavDropdown title="Admin" id="basic-nav-dropdown">
-                        <MenuItem >Action</MenuItem>
-                        <MenuItem >Another action</MenuItem>
-                        <li ><Link to='/'>Home</Link></li>
-                        <MenuItem divider />
-                        <MenuItem >Separated link</MenuItem>
-                    </NavDropdown>
+                    
                     </Nav>
                     {this.props.user ? <HeaderOut user={this.props.user} onLogout={this.props.onLogout}/> : <HeaderIn onAuth={this.props.onAuth}/> }
+
                 </Navbar.Collapse>
                 </Navbar>
             
---
+++ frontend/src/components/newapp.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import Header from './Global/Header';
+import Body from './Global/Body';
+import firebase from 'firebase';
+import routes from './routes'
+
+
+class App extends Component {
+  constructor () {
+    super()
+    this.handleAuth = this.handleAuth.bind(this)
+    this.handleLogout = this.handleLogout.bind(this)
+    this.getRol = this.getRol.bind(this)
+    this.routesFilter = this.routesFilter.bind(this)
+  }
+
+  state = {
+    user: null,
+    rol: "student",
+    routes: routes,
+    routesFiltered:[],
+  }
+
+  getRol(){
+
+  }
+  routesFilter(rol){
+    let newRoutesFiltered = this.state.routes.filter(function(route){
+      return(route.rol === rol)
+    });
+    this.setState({
+      routesFiltered: newRoutesFiltered,
+    });
+  }
+
+  componentWillMount () {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ user });
+      console.log(user);
+    })
+    this.getRol();
+    this.routesFilter(this.state.rol);
+  }
+
+  handleAuth () {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    provider.addScope('https://www.googleapis.com/auth/plus.login')
+
+
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user} ha iniciado sesión`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`))
+  }
+
+  handleLogout () {
+    firebase.auth().signOut()
+      .then(result => console.log('Te has desconectado correctamente'))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`))
+  }
+
+
+  
+  render() {
+    console.log(this.state)
+    return (
+      <div className="App">
+        <Header
+        user={this.state.user}
+        onAuth={this.handleAuth}
+        onLogout={this.handleLogout}
+        routes={this.state.routesFiltered}
+        />
+        <Body user={this.state.user} routes={this.state.routesFiltered}/>
+      </div>
+    
+    );
+  }
+}
+
+export default App;
