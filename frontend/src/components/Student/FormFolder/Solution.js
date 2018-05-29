4f334357b2902a8929d11260cf2bb8e6ce0769de
Unknown <leiser.mahu@gmail.com>
2018-05-29 13:18:04 -0400

    React-ace basico

--- frontend/src/components/Student/FormFolder/Solution.js
+++ frontend/src/components/Student/FormFolder/Solution.js
@@ -4,6 +4,14 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import ThemeDefault from '../ThemeList';
 import Paper from 'material-ui/Paper';
 import {Link} from 'react-router-dom';
+import {Grid,Row,Col} from 'react-bootstrap'
+import Drawer from 'material-ui/Drawer';
+import AppBar from 'material-ui/AppBar';
+import { white } from 'material-ui/styles/colors';
+import AceEditor from 'react-ace';
+
+import python from 'brace/mode/python';
+import monokai from 'brace/theme/monokai';
 const background = {
   bigFrame:{
     padding: 30,
@@ -14,8 +22,13 @@ const background = {
     height: 300
 
   },
+  textAreaStyle2:{
+    height: 400,
+      width:450
+  },
   titleStyle:{
-    width: 600
+    width: 600,
+    textAlign:"center"
   }
 }
 class Solution extends Component {
@@ -25,10 +38,20 @@ class Solution extends Component {
         exercise: props.location.state,
         title: null,
         description: null,
-        isSafeToRender: false
+        isSafeToRender: false,
+        sideMenu:false
       }
+      this.handleToggle = this.handleToggle.bind(this)
+      this.function = this.function.bind(this)
 
     }
+  handleToggle(){
+    this.setState({sideMenu: !this.state.sideMenu});
+  }
+  function(){
+    return <p style={{color: white}}>AQUI VA ALGO DINAMICO</p>
+  }
+
 
   componentWillMount () {
     console.log(this.state.exercise)
@@ -82,7 +105,69 @@ class Solution extends Component {
                               </Link>
 
                         </Form>
+                        <Grid>
+                        <Row className="show-grid" style={{position:"relative", left: "5%"}}>
+                          <Col lg={5}>
+                          <label>Descripcion</label>
+                          <AceEditor
+                                style = {background.textAreaStyle2}
+                                mode={'python'}
+                                theme={'monokai'}
+                                name="blah2"
+                                fontSize={17}
+                                showPrintMargin={true}
+                                showGutter={true}
+                                highlightActiveLine={true}
+                                value={""}
+                                setOptions={{
+                                    enableBasicAutocompletion: true,
+                                    enableLiveAutocompletion: true,
+                                    enableSnippets: true,
+                                    showLineNumbers: true,
+                                    tabSize: 2,
+                                }}/>
+                
+                          </Col>
+                          <Col lg={1}>
+                              <Row className="show-grid">
+                              <div style={{padding:30}}></div>
+                              <Link to={{ pathname: '/exercises_student' }}>
+                                    <Button primary={true} type='Back'>
+                                              Ejecutar
+                                      </Button>
+                              </Link>
+                              </Row>
+
+                              <Row className="show-grid">
+
+                              <div style={{padding:30}}></div>
+                             
+                                    <Button primary={true} type='Back'
+                                            onClick={this.handleToggle}
+                                            >
+                                              Enviar
+                                      </Button>
+                              </Row>
+
+                          </Col>
+                          <Col lg={5}>
+                          <label>Descripcion</label>
+                                  <TextArea   placeholder='Descripcion'
+                                              value= {this.state.description} 
+                                              style={background.textAreaStyle2}  
+                                              />  
+                          </Col>
+                         
+                          </Row>
+                        </Grid>
                      </Paper>
+                     <Drawer width={400} style = {{backgroundColor: white}}
+                              openSecondary={true} open={this.state.sideMenu} >
+                        <AppBar title="AppBar"    
+                                iconElementLeft={false}
+                              />
+                              {this.function()}
+                      </Drawer>
 
                 </MuiThemeProvider>
           
@@ -98,4 +183,31 @@ class Solution extends Component {
   }
   
   export default Solution;
-  
+  /* <AceEditor
+                    mode= {this.state.lenguajeElegido} //escoger lenguaje
+                    theme="terminal"
+                    name="blah2"
+                    //onChange={this.updateCodigoA.bind(this)}
+                    onChange={this.onChange}
+                    fontSize={18}
+                    showPrintMargin={true}
+                    showGutter={true}
+                    highlightActiveLine={true}
+                    value = ""
+                    setOptions={{
+                        enableBasicAutocompletion: true,
+                        enableLiveAutocompletion: false,
+                        enableSnippets: false,
+                        showLineNumbers: true,
+                        tabSize: 2,
+                    }}/>
+                </div>
+                
+                  <TextArea   placeholder='Descripcion'
+                                              value= {"Aca va el react-ace"} 
+                                              style={background.textAreaStyle2}  
+                                              />  
+                
+                
+                
+                */
