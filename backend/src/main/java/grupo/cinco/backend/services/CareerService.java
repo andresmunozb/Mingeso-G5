baf04a60f2666399fa300879ec8925cb2124c35b
Unknown <andres.munoz.b@usach.cl>
2018-05-29 12:53:55 -0400

    Servicios de Career y Class

--- backend/src/main/java/grupo/cinco/backend/services/CareerService.java
+++ backend/src/main/java/grupo/cinco/backend/services/CareerService.java
@@ -2,6 +2,7 @@ package grupo.cinco.backend.services;
 
 
 import grupo.cinco.backend.entities.Career;
+import grupo.cinco.backend.entities.User;
 import grupo.cinco.backend.repositories.CareerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,8 +18,15 @@ public class CareerService {
 
     @RequestMapping(value = "/",method = RequestMethod.GET)
     @ResponseBody
-    public Iterable<Career> getAllProducts() {
+    public Iterable<Career> getAllCareers() {
         return careerRepository.findAll();
+    }
+
+    @RequestMapping(value = "/{id}/users",method = RequestMethod.GET)
+    @ResponseBody
+    public Iterable<User> getUsers(@PathVariable("id") Integer id) {
+        Career career = careerRepository.findById(id).get();
+        return career.getUsers();
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
@@ -27,6 +35,22 @@ public class CareerService {
     public Career create(@RequestBody Career resource)
     {
         return careerRepository.save(resource);
+    }
+
+    @RequestMapping(value = "/{id}/edit", method = RequestMethod.PUT)
+    @ResponseBody
+    public void update(@PathVariable("id") Integer id, @RequestBody Career resource)
+    {
+        Career career = careerRepository.findById(id).get();
+        career.setName(resource.getName());
+        careerRepository.save(career);
+    }
+    @DeleteMapping(value = "{id}/delete")
+    @ResponseBody
+    public void delete(@PathVariable Integer id)
+    {
+        Career career = careerRepository.findById(id).get();
+        careerRepository.delete(career);
     }
 
 }
--- backend/src/main/java/grupo/cinco/backend/services/ClassService.java
+++ backend/src/main/java/grupo/cinco/backend/services/ClassService.java
@@ -2,6 +2,7 @@ package grupo.cinco.backend.services;
 
 
 import grupo.cinco.backend.entities.Class;
+import grupo.cinco.backend.entities.User;
 import grupo.cinco.backend.repositories.ClassRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,11 +23,34 @@ public class ClassService {
         return classRepository.findAll();
     }
 
+    @RequestMapping(value = "/{id}/users",method = RequestMethod.GET)
+    @ResponseBody
+    public Iterable<User> getUsers(@PathVariable("id") Integer id) {
+        Class clase = classRepository.findById(id).get();
+        return clase.getUsers();
+    }
+
     @RequestMapping(value = "/create",method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
     public Class create(@RequestBody Class resource)
     {
         return classRepository.save(resource);
+    }
+
+    @RequestMapping(value = "/{id}/edit", method = RequestMethod.PUT)
+    @ResponseBody
+    public void update(@PathVariable("id") Integer id, @RequestBody Class resource)
+    {
+        Class clase = classRepository.findById(id).get();
+        clase.setName(resource.getName());
+        classRepository.save(clase);
+    }
+    @DeleteMapping(value = "{id}/delete")
+    @ResponseBody
+    public void delete(@PathVariable Integer id)
+    {
+        Class career = classRepository.findById(id).get();
+        classRepository.delete(career);
     }
 }
